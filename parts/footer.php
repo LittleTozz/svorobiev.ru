feadf0f2c79149275b3490b5a341fbf2cec0ea35
Sergey Vorobiev <54852578+LittleTozz@users.noreply.github.com>
2023-04-01 18:00:57 +0300

    Update footer.php
    Вставил код yandex метрики

--- parts/footer.php
+++ parts/footer.php
@@ -2,3 +2,6 @@
     <p class="footer__content">Найдите меня на других площадках: <a href="https://vk.com/littletoss">VK</a>, <a href="https://t.me/LittleToss">Telegram</a>. Напишите мне на электронную почту <a href="mailto:geroy2001@gmail.com">geroy2001@gmail.com</a>,
         особенно, если речь идет о создание сайта, починки компьютера, игре на гитаре, спорте. Этот сайт был переработан в 2023 году. Можете посмотреть мой старый сайт <a href="/oldsite">здесь</a>.</p>
 </div>
+<!-- Yandex.Metrika counter -->
+    <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(82935874, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/82935874" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
