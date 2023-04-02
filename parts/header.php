78f99e7e1bb1696eb230d5c9b9cde595211754e3
Sergey Vorobiev <54852578+sergey-vorobiev@users.noreply.github.com>
2023-04-02 20:58:30 +0300

    Разобрался с активными ссылками в мнею

--- parts/header.php
+++ parts/header.php
@@ -9,11 +9,11 @@ $menu_links = array(
 
 <div class="header container">
     <div class="header__logo">
-        <a href="/blog">Воробьев Сергей</a>
+        <a href="/"<?php if ($current_url == "/") { echo(' class="active"'); } ?>>Воробьев Сергей</a>
     </div>
     <div class="header__menu">
         <?php foreach ($menu_links as $url => $title) { ?>
-            <a href="<?php echo $url; ?>" class="header__menu-link<?php echo strpos($current_url, $url) !== false ? ' active' : ''; ?>"><?php echo $title; ?></a>
+            <a href="<?php echo $url; ?>" class="header__menu-link<?php if ($current_url == $url || $current_url == $url . '/') { echo(' active');} ?>"><?php echo $title; ?></a>
         <?php } ?>
     </div>
 </div>
